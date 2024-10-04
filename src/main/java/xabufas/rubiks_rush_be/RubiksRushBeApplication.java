6366339bfe4b2c9f0c4cc3ec1c729858026a0174
Xavier Fernandes <xavier.fernandes@vwds.pt>
2024-10-04 19:44:16 +0100

    🎉 kickstart project from spring initializr

---
+++ src/main/java/xabufas/rubiks_rush_be/RubiksRushBeApplication.java
@@ -0,0 +1,13 @@
+package xabufas.rubiks_rush_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RubiksRushBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RubiksRushBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xabufas/rubiks_rush_be/RubiksRushBeApplicationTests.java
@@ -0,0 +1,13 @@
+package xabufas.rubiks_rush_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RubiksRushBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
